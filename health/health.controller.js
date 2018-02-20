5c0c5b31d0ffc471f6309dc49d08b4aed6a7f3f8
Chris Paskvan <chris.paskvan@apricothill.com>
2018-02-20 17:24:27 -0600

    hotfix(version-update): 2.0.11 (Garden Progeny 1) (#20)

--- health/health.controller.js
+++ health/health.controller.js
@@ -126,7 +126,7 @@ class HealthController {
 			await this.worldRepository.open(worldDatabasePath);
 
 			const [{ displayProperties: { description = notAvailable }}] =
-				await this.worldRepository.getItemByName('Crimil\'s Dagger');
+				await this.worldRepository.getItemByName('Garden Progeny 1');
 			await this.worldRepository.close();
 
 			return description;
