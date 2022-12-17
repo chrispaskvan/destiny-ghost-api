d31166305e18d49b0a144d35c3c5317847c12618
Chris Paskvan <chris.paskvan@apricothill.com>
2022-12-17 15:24:19 -0500

    feat(experimental-specifier-resolution): Remove flag in preparation for Node >= 19. (#256)

---
+++ loader.js
@@ -0,0 +1,51 @@
+/**
+ * @see {@link https://github.com/nodejs/loaders-test/tree/main/commonjs-extension-resolution-loader}
+ */
+import { isBuiltin } from 'node:module';
+import { dirname } from 'node:path';
+import { cwd } from 'node:process';
+import { fileURLToPath, pathToFileURL } from 'node:url';
+import { promisify } from 'node:util';
+
+// eslint-disable-next-line import/extensions
+import resolveCallback from 'resolve/async.js';
+
+const resolveAsync = promisify(resolveCallback);
+
+const baseURL = pathToFileURL(`${cwd()}/`).href;
+
+// eslint-disable-next-line import/prefer-default-export
+export async function resolve(specifier, context, next) {
+    const { parentURL = baseURL } = context;
+
+    if (isBuiltin(specifier)) {
+        return next(specifier, context);
+    }
+
+    // `resolveAsync` works with paths, not URLs
+    if (specifier.startsWith('file://')) {
+        // eslint-disable-next-line no-param-reassign
+        specifier = fileURLToPath(specifier);
+    }
+    const parentPath = fileURLToPath(parentURL);
+
+    let url;
+    try {
+        const resolution = await resolveAsync(specifier, {
+            basedir: dirname(parentPath),
+            // eslint-disable-next-line max-len
+            // For whatever reason, --experimental-specifier-resolution=node doesn't search for .mjs extensions
+            // but it does search for index.mjs files within directories
+            extensions: ['.js', '.json', '.node', '.mjs'],
+        });
+        url = pathToFileURL(resolution).href;
+    } catch (error) {
+        if (error.code === 'MODULE_NOT_FOUND') {
+            // Match Node's error code
+            error.code = 'ERR_MODULE_NOT_FOUND';
+        }
+        throw error;
+    }
+
+    return next(url, context);
+}
