b481c2e84616c618537db1a1b82b19d0c034843b
chrispaskvan <chris.paskvan@apricothill.com>
2023-07-16 18:55:58 -0400

    feat(update-azure): Upgrade to latest Azure SDK and add observability to Redis.

--- helpers/cache.js
+++ helpers/cache.js
@@ -1,10 +1,12 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import Redis from 'ioredis';
 import configuration from './config';
+import log from './log';
 
 const { redis } = configuration;
 /**
  * Cache Store
+ * @description Client includes built-in reconnect strategy. {@link https://github.com/redis/ioredis#auto-reconnect}
  */
 const redisConfiguration = {
     host: redis.host,
@@ -15,5 +17,8 @@ const redisConfiguration = {
     },
 };
 const client = new Redis(redisConfiguration);
+
+client.on('connect', () => log.info('Redis Client is connected.'));
+client.on('error', err => log.error({ err }, 'Connection to the Redis Server failed.'));
 
 export default client;
--- vitest.config.js
+++ vitest.config.js
@@ -5,6 +5,11 @@ export default defineConfig({
     test: {
         coverage: {
             reporter: ['clover', 'html'],
+            statements: 79.31,
+            branches: 88.35,
+            functions: 70.7,
+            lines: 79.31,
+            thresholdAutoUpdate: true,
         },
     },
 });
