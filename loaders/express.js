17ebe6db99d82807b1eaa85bc62f7214b1c971f8
chrispaskvan <chris.paskvan@apricothill.com>
2023-10-28 21:37:37 -0400

    Add request/response timeout to express and uncaught exception/unhandled rejection errors.

--- loaders/express.js
+++ loaders/express.js
@@ -95,4 +95,13 @@ export default app => {
      * Rate Limiter
      */
     app.use(rateLimiterMiddleware);
+
+    /**
+     * Request/Response Timeouts
+     */
+    app.use((req, res, next) => {
+        req.setTimeout(5000); // Set request timeout to 5 seconds
+        res.setTimeout(5000); // Set response timeout to 5 seconds
+        next();
+    });
 };
--- start.js
+++ start.js
@@ -1,12 +1,25 @@
 import { startServer } from './server';
 
+function exitOnError(err) {
+    // eslint-disable-next-line no-console
+    console.log(err);
+    process.exit(1);
+}
+
 async function start() {
     await startServer();
 }
 
+process.on('unhandledRejection', reason => {
+    // throw and let the uncaughtException handler handle it
+    throw reason;
+});
+
+process.on('uncaughtException', err => {
+    exitOnError(err);
+});
+
 start()
     .catch(err => {
-        // eslint-disable-next-line no-console
-        console.log(err);
-        process.exit(1);
+        exitOnError(err);
     });
